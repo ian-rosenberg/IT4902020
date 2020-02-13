449de96787d73f7df95156829839322a873aed9d
David Dembner <davidtdembner@gmail.com>
2020-02-13 13:15:45 -0500

    server/client can now connect to database

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -6,12 +6,17 @@ require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 
+if(!$argv[1] || !$argv[2]){
+	echo "Hey dummy you forgot them arguments".PHP_EOL;
+     	exit(1);
 
+}	
 $request = array();
 $request['type'] = "login";
 $request['username'] = $argv[1];
 $request['password'] = $argv[2];
 $request['message'] = "HI";
+
 $response = $client->send_request($request);
 $response = $client->publish($request);
 
