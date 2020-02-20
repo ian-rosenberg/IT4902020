1b77df9c20c6d29932bfda6f17e4a132bf272c12
David Dembner <davidtdembner@gmail.com>
2020-02-20 11:24:03 -0500

    made new client class

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -5,12 +5,7 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-
-if(!$argv[1] || !$argv[2]){
-	echo "Hey dummy you forgot them arguments".PHP_EOL;
-     	exit(1);
-
-}	
+	
 $request = array();
 $request['type'] = "login";
 $request['username'] = $argv[1];
@@ -18,7 +13,7 @@ $request['password'] = $argv[2];
 $request['message'] = "HI";
 
 $response = $client->send_request($request);
-$response = $client->publish($request);
+//$response = $client->publish($request);
 
 echo "client received response: ".PHP_EOL;
 print_r($response);
