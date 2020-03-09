a28af2b54820dca97ae5b844706932fdbe555fb5
Brandon <bw237@njit.edu>
2020-03-09 17:14:08 -0400

    modified:   bmicalcstyle.css 	modified:   index.php

--- bmicalc/index.php
+++ bmicalc/index.php
@@ -3,64 +3,58 @@
 if(isset($_POST['subdata']) && ($_POST['subdata']) == "Submit"){
 	extract($_POST);
 
-	if($unit == 'Standard'){
-		$heightsquared = $height * $height;
-		$bmicalc = ($weight / $heightsquared) * 703;
-		$bmi = number_format($bmicalc, 1);
+	if($unit != 'pick' && $weight != '' && $height != ''){
+		if($unit == 'Standard'){
+			$heightsquared = $height * $height;
+			$bmicalc = ($weight / $heightsquared) * 703;
+			$bmi = number_format($bmicalc, 1);
 
-		echo "<center>";
-		echo "Your BMI is $bmi. ";
+			$result = "Your BMI is $bmi. ";
 
-		if($bmi > '18.5' && $bmi < '25'){
-			echo "You are considered within the normal BMI range.<br /><br />";
+			if($bmi > '18.5' && $bmi < '25'){
+				$result .= "You are considered within the normal BMI range.";
+			}
+			elseif($bmi < '18.5'){
+				$result .= "You are considered underweight.";
+			}
+			elseif($bmi >= '25' and $bmi < '30'){
+				$result .= "You are considered overweight.";
+			}
+			elseif($bmi >= '30' and $bmi < '40'){
+				$result .= "You are considered obese.";
+			}
+			elseif($bmi >= 40){
+				$result .= "You are considered extremely obese.";
+			}
 		}
-		elseif($bmi < '18.5'){
-			echo "You are considered underweight.<br /><br />";
-		}
-		elseif($bmi >= '25' and $bmi < '30'){
-			echo "You are considered overweight.<br /><br />";
-		}
-		elseif($bmi >= '30' and $bmi < '40'){
-			echo "You are considered obese.<br /><br ?>";
-		}
-		elseif($bmi >= 40){
-			echo "You are considered extremely obese.<br /><br />";
-		}
-		elseif($bmi == '69'){
-			echo "DAAAAAAAAAAANGGGGG BRO.<br /><br />";
-		}
+		elseif($unit == 'Metric'){
+			$heightmet = $height / 100;
+			$heightsquared = $heightmet * $heightmet;
+			$bmicalc = $weight / $heightsquared;
+			$bmi = number_format($bmicalc, 1);
 
-		echo "</center>";
+			$result = "Your BMI is $bmi. ";
+
+			if($bmi > '18.5' && $bmi < '25'){
+				$result .= "You are considered within the normal BMI range.";
+			}
+			elseif($bmi < '18.5'){
+				$result .= "You are considered underweight.";
+			}
+			elseif($bmi >= '25' and $bmi < '30'){
+				$result .= "You are considered overweight.";
+			}       
+			elseif($bmi >= '30' and $bmi < '40'){
+				$result .= "You are considered obese.";
+			}       
+			elseif($bmi >= 40){
+				$result .= "You are considered extremely obese.";
+			}       
+		}
 	}
-	elseif($unit == 'Metric'){
-		$heightmet = $height / 100;
-		$heightsquared = $heightmet * $heightmet;
-		$bmicalc = $weight / $heightsquared;
-		$bmi = number_format($bmicalc, 1);
-
-		echo "<center>";
-		echo "Your BMI is $bmi. ";
-
-		if($bmi > '18.5' && $bmi < '25'){
-			echo "You are considered within the normal BMI range.<br /><br />";
-		}
-		elseif($bmi < '18.5'){
-			echo "You are considered underweight.<br /><br />";
-		}
-		elseif($bmi >= '25' and $bmi < '30'){
-                        echo "You are considered overweight.<br /><br />";
-                }       
-                elseif($bmi >= '30' and $bmi < '40'){
-                        echo "You are considered obese.<br /><br ?>";
-                }       
-                elseif($bmi >= 40){
-                        echo "You are considered extremely obese.<br /><br />";
-                }       
-                elseif($bmi == '69'){
-			echo "DAAAAAAAAAAANGGGGG BRO.<br /><br />";
-		}
-
-		echo "</center>";
+	elseif($unit == 'pick' || empty($weight) || empty($height))
+	{
+		$result = "Please fill out everything.";
 	}
 }
 
@@ -85,9 +79,10 @@ if(isset($_POST['subdata']) && ($_POST['subdata']) == "Submit"){
 					</select><br /><br />
                                 Weight: <input type="number" name="weight" placeholder="(In lbs or kg)"> <br /><br />
                                 Height: <input type="number" name="height" placeholder="(In inches or cm)"> <br /><br />
-                                <input type="submit" name="subdata" id="subdata" class="btn" value="Submit"/>
+				<input type="submit" name="subdata" id="subdata" class="btn" value="Submit"/><br /><br />
+				Result: <?php echo $result; ?> 
                         </form>
-                </div>
+		</div>
         </body>
 </html>
 
