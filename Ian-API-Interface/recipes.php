5a9916ca9218c426543597119ce8697975ef1b48
ian <isr5@njit.edu>
2020-03-11 14:28:23 -0400

    Recipe like/dislike in progress

---
+++ Ian-API-Interface/recipes.php
@@ -0,0 +1,140 @@
+<?php
+require_once('rabbitMQLib.inc');
+require_once("sendDisLog.php");
+
+class APIAccess
+{
+	private $client;
+
+	public function __construct()
+	{
+		$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+	}		
+
+	public function SendRandomRecipe($filename)
+	{
+		
+	}
+
+	public function SaveImage($img, $path)
+	{
+		$ch = curl_init ($img);
+		
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1 );
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+		
+		$rawData = curl_exec($ch);
+		
+		curl_close($ch);
+		
+		if(file_exists($path))
+		{
+			unlink($path);
+		}
+		
+		$f = fopen($path, 'x');
+		fwrite($f, $rawData);
+		fclose($f);
+		
+		return $path.$img;
+	}
+
+	public function GetRandomRecipes($tags, $num)
+	{
+		$comma = "%252C";
+		$tagField = "&tags=";
+		
+		$url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=".$num;
+		
+		if(strstr($tags, ',') and !empty($tags))
+		{		
+			$tagsExploded = explode(',', $tags);
+						
+			if(count($tagsExploded))
+			{
+				$url .= $tagField;
+			}
+						
+			foreach($tag  as $tagsExploded)
+			{
+				if($tag == end($tagsExploded))
+				{
+					$url = $url . $tag;
+				}
+				else
+				{
+					$url = $url . $tag . $comma;
+				}
+			}
+		}
+		
+		echo($url);
+
+		sendToLogger("using $url");
+		
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "GET",
+			CURLOPT_HTTPHEADER => array(
+				"x-rapidapi-host: spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+				"x-rapidapi-key: 121607682cmsh2a3a276639a2519p11fb8bjsn875259b25ff8",
+				"Content-Type: application/json"
+			),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+			echo "cURL Error #:" . $err.PHP_EOL;
+		} else {
+			sendToLogger($response);
+			
+			return $response;
+		}
+	}
+}
+
+$api = new APIAccess();
+$response = "";
+
+if(!empty($argv))
+{
+	$response = $api->GetRandomRecipes(implode(',',array_slice($argv, 2)), $argv[1]);
+}
+else
+{
+	$response = $api->GetRandomRecipes("", 1);	
+}
+
+sendToLogger($response.PHP_EOL.PHP_EOL);
+
+$json =json_decode($response, true);
+
+$img = $json['recipes'][0]['image'];
+$imgName  = $json['recipes'][0]['title'];
+
+sendToLogger($img.PHP_EOL.PHP_EOL);
+		 
+$saveResponse = $api->SaveImage($img, "RecipeIcons/".$imgName);
+
+if(!empty($saveResponse))
+{
+	sendToLogger("Saved image ".$img.PHP_EOL);
+}
+else
+{
+	sendToLogger("Failed to save image ". $img.PHP_EOL);
+}
+?>
