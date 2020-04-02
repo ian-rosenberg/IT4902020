89df82e38b3af8e7d20e9be9aedf9861adfe1525
rjs76 <35549025+rjs76@users.noreply.github.com>
2020-04-02 02:49:13 -0400

    updating dmzservice with images showing recipes

--- dmzService/dmzServer.php
+++ dmzService/dmzServer.php
@@ -39,14 +39,62 @@ function requestProcessor($request)
 		echo $response;
 	}	
 	return($response);
+	}
+	$recipesid = getRecipeid(json_decode($response, true));
+	$recipes = getRecipes($recipesNames);
+		
+	return($recipes);
+}
+
+function getRecipeid($recipes){
+	
+	$recipeid = array();
+	$index = 0;
+	for($i = 0; $i < 3; $i++) {
+		$recipeid[$i] = $recipes['meals'][$i]['id'];
+	}
+	
+	return $recipeid;
+}
+
+function getRecipes($recipeid){
+	
+	for($i=0; $i <=2; $i++){
+		
+		$recipeid[i];
+		$curl_url= "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+recipeid[i]+"/information?includeNutrition=false", 
+		}	
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/505163/information?includeNutrition=false",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => array(
+		"x-rapidapi-host: spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		"x-rapidapi-key: 121607682cmsh2a3a276639a2519p11fb8bjsn875259b25ff8"
+	),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
+}
+	return($response);
 }
 
 #Second parameter must match info from testRabbitMQ.ini
 $server = new rabbitMQServer("testRabbitMQ.ini", "dmzServer");
 
 $server->process_requests('requestProcessor');
-
-
-
 
 ?>
