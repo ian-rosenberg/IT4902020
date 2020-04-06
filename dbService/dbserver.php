6564cbcebf1636c00ff1ddb3df773e7667bd3346
Ubuntu <ubuntu@ip-10-0-1-40.us-east-2.compute.internal>
2020-04-06 21:49:48 +0000

    dbserver query for restrictions added to register

--- dbService/dbserver.php
+++ dbService/dbserver.php
@@ -10,6 +10,61 @@ echo "Now Listening for rabbitMQ messages".PHP_EOL;
 
 echo SendToLogger("DB Server start");
 
+function recipes($arr, $db){
+	$title = $arr['title'];
+        $url = $arr['spoonacularSourceUrl'];
+        $id = $arr['id'];
+        //$imageType = $arr['imageType'];
+        $imageUrl = "https://spoonacular.com/recipeImages/$id-240x150.jpg";
+        $calories = $arr['nutrition']['nutrients'][0]['amount'];
+
+        $query = "insert into recipes(recipes_name, calories, url, image, recipe_id) values('$title', $calories, '$url', '$imageUrl', $id)";
+	$db->query($query);
+	if($db->affected_rows != 0){
+        	return 1;
+        }
+        else{
+                return -1;
+        }
+}
+
+function restrictions($arr, $db){
+	$vegetarian = (int)$arr['vegetarian'];
+        $vegan = (int)$arr['vegan'];
+        $gluten = (int)$arr['glutenFree'];
+	$dairy = (int)$arr['dairyFree'];
+	$id = $arr['id'];
+
+	$query = "insert into recipe_restrictions(id, vegetarian, vegan, glutenFree, dairyFree) select recipes.id, $vegetarian, $vegan, $gluten, $dairy from recipes where recipes.recipe_id = $id";
+
+	$db->query($query);
+        if($db->affected_rows != 0){
+                return 1;
+        }
+        else{
+                return -1;
+        }
+}
+
+function ingredients($arr, $db){
+	for($i = 0; $i < count($arr['extendedIngredients']); $i++){
+		$name = $arr['extendedIngredients'][$i]['name'];
+		$amount = $arr['extendedIngredients'][$i]['amount'];
+		$unit = $arr['extendedIngredients'][$i]['unit'];
+		$id = $arr['id'];
+
+		$query = "insert into ingredients(id, ingredients_name, amount, measurement) select recipes.id, '$name', $amount, '$unit' from recipes where recipes.recipe_id = $id";
+		$db->query($query);        
+	}
+	if($db->affected_rows != 0){
+        	return 1;
+        }
+        else{
+                return -1;
+        }
+
+}
+
 function requestProcessor($request)
 {
 
@@ -72,8 +127,10 @@ function requestProcessor($request)
 					$username=$request['username'];
 					$query1="insert into bmi(id) select login.id from login where login.username='$username'";
 					$query2="insert into calbudget(id) select login.id from login where login.username='$username'";
+					$query3="insert into restrictions(id) select login.id from login where login.username='$username'";
 					$db->query($query1);
 					$db->query($query2);
+					$db->query($query3);
 					return 1;
 				}
 				else{
@@ -112,9 +169,49 @@ function requestProcessor($request)
 				return $e->getMessage();
 			}
 		case 'DMZ':
+			$db = new mysqli("127.0.0.1", "brandon", "admin", "it490");
+			
 			for($i=0; $i < 3; $i++){
 				$arr = json_decode($request[$i], true);
-				var_dump($arr);
+				//var_dump($arr);
+				//$dump = $arr['extendedIngredients'];
+				//echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+				//var_dump(count($dump));
+
+				if(recipes($arr, $db) == -1){
+					continue;
+				}
+				elseif(restrictions($arr, $db) == -1){
+					continue;	
+				}
+				elseif(ingredients($arr, $db) == -1){
+					continue;
+				}
+
+				/**$title = $arr['title'];
+				$url = $arr['spoonacularSourceUrl'];
+				$id = $arr['id'];
+				$imageType = $arr['imageType'];
+				$imageUrl = "https://spoonacular.com/recipeImages/$id-240x150.jpg";
+				$calories = $arr['nutrition']['nutrients'][0]['amount'];
+				$vegetarian = $arr['vegetarian'];
+				$vegan = $arr['vegan'];
+				$gluten = $arr['glutenFree'];
+				$dairy = $arr['dairyFree'];
+
+				$recipe = "insert into recipes(name, calories, url, image, recipe_id) values('$title', $calories, '$url', '$imageUrl', $id)"; 
+				$restrictions = "insert into recipe_restrictions(vegetarian, vegan, glutenFree, dairyFree) values ('$vegetarian', '$vegan', '$gluten', '$dairy')";
+				$ingredients
+
+				$response1 = $db->query($recipe);
+				$response2 = $db->query($restrictions);
+				//$response3 = $db->query($ingredients);
+
+				var_dump($response1);
+				var_dump($response2);
+				var_dump($response3);
+				echo "$title, $url, $id, $imageType, $imageUrl, $calories";*/
+
 			}
 			return 1;
 
