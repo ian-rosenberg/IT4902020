67b9340815fac13dfda79dc44d520ef942dea0d5
Ubuntu <ubuntu@ip-10-0-1-10.us-east-2.compute.internal>
2020-05-07 23:43:22 +0000

    Front end deployment

--- WebServer/frontEndDeploymentClient.php
+++ WebServer/frontEndDeploymentClient.php
@@ -3,44 +3,33 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('package.php.inc');
-require_once('sendDisLog.php');
 
-SendToLogger("Starting update for front-end".PHP_EOL);
 echo "Starting update for front-end".PHP_EOL;
 
-$filesArray = Package::CONSTANT;
-$filesString = "";
-for($i = 0; $i < count($filesArray); $i++){
-        $filesString .= $filesArray[$i] . ' ';
-}
+exec("ls -t | head -1  ../packages", $output);
 
-exec("ls packages", $output);
-
-$filename = "frontendPackage.1";
+$filename = "frontendPackage.";
 $version = "1";
 
 if(!empty($output))
 {
-        $filename = $output[count($output)-1];
+        $exploded = explode('.', $output[0]);
 
-	$fileVersion = explode('.', $filename);
+	$version = $exploded[1];
 
-	$version = (int)$fileVersion[1];
 	$version += 1;
-
-	$filename = $fileVersion[0] . "." . $version;
 }
 
-SendToLogger("Current version: ". $version.PHP_EOL);
+$filename = $filename . "." . $version;
+
+
 echo "Current version: ". $version.PHP_EOL;
 
 exec("tar -czvf $filename.tar.gz /home/ubuntu/git/IT4902020/WebServer", $output, $return_val);
-exec("mv  $filename.tar.gz packages/");
-exec("scp packages/$filename.tar.gz ubuntu@10.0.1.40:~/git/IT4902020/deployment/frontEnd/");
+exec("mv  $filename.tar.gz ..packages/");
+exec("scp ../packages/$filename.tar.gz ubuntu@10.0.1.40:~/git/IT4902020/deployment/frontEnd/");
 
 print_r($output);
-SendToLogger($output);
 
 $client = new rabbitMQClient("testRabbitMQ.ini","brandonServer");
 
@@ -52,11 +41,8 @@ $request['version'] = $version;
 $response = $client->send_request($request);
 
 echo "client received response: ".PHP_EOL;
-SendToLogger("client received response: ".PHP_EOL);
 
 print_r($response);
 echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
-SendToLogger($argv[0]." END".PHP_EOL);
-
