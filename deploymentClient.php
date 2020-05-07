b2359d1e53058d92e54f4d7a1c70725e0154a80c
Ubuntu <ubuntu@ip-10-0-1-140.us-east-2.compute.internal>
2020-05-07 21:05:23 +0000

    fixed version numbering

--- deploymentClient.php
+++ deploymentClient.php
@@ -6,28 +6,50 @@ require_once('rabbitMQLib.inc');
 require_once('package.php.inc');
 
 
+function GetLatestFileVersion($files){
+
+	$newestVersion = 0;
+	for($i = 0; $i < count($files); $i++){
+		$file = explode('.', $files[$i]);
+		$version = $file[1];
+		if($version > $newestVersion){
+			$newestVersion = $version;
+		}
+	}
+	return $newestVersion;
+}
+
 $filesArray = Package::CONSTANT;
 $filesString = "";
 for($i = 0; $i < count($filesArray); $i++){
 	$filesString .= $filesArray[$i] . ' ';
 }
 
-$fileName = "rabbitMQPackage.2.0.tar.gz";
-exec("tar -czf " . $fileName . " " . $filesString, $output, $return_val);
-exec("mv $fileName packages/");
-exec("scp packages/$fileName ubuntu@10.0.1.40:~/git/IT4902020/deployment/rabbitMQ");
-
 exec("ls packages", $output);
 
-print_r($output);
+$fileName = "rabbitMQPackage.1.+.tar.gz";
+
+if(!empty($output)){
+
+	
+	$latestVersion = GetLatestFileVersion($output);
+	$latestVersion++;
+	$fileName = "rabbitMQPackage." . $latestVersion . ".+.tar.gz";
+}
+ 
+exec("tar -czf " . $fileName . " " . $filesString, $output, $return_val);
+exec("mv $fileName packages/");
+echo $fileName;
+exit();
+exec("scp packages/$fileName ubuntu@10.0.1.40:~/git/IT4902020/deployment/rabbitMQ");
 
 $client = new rabbitMQClient("testRabbitMQ.ini","brandonServer");
 
 $request = array();
 $request['type'] = "update";
 $request['server'] = "rabbitMQ";
-$request['message'] = "Hey Brandon";
-print($request['message']);
+$request['version'] = $latestVersion;
+
 $response = $client->send_request($request);
 
 echo "client received response: ".PHP_EOL;
