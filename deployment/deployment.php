84ceaa58a4cb63b1807024d4dc0e0d1001b4ac89
Ubuntu <ubuntu@ip-10-0-1-40.us-east-2.compute.internal>
2020-05-10 07:41:27 +0000

    modified:   deployment.php 	deleted:    rabbitMQ/rabbitMQPackage.1.+.tar.gz 	deleted:    rabbitMQ/rabbitMQPackage.10.+.tar.gz 	deleted:    rabbitMQ/rabbitMQPackage.11.+.tar.gz 	deleted:    rabbitMQ/rabbitMQPackage.2.+.tar.gz 	deleted:    rabbitMQ/rabbitMQPackage.3.+.tar.gz 	deleted:    rabbitMQ/rabbitMQPackage.4.+.tar.gz 	deleted:    rabbitMQ/rabbitMQPackage.5.+.tar.gz 	deleted:    rabbitMQ/rabbitMQPackage.6.+.tar.gz 	deleted:    rabbitMQ/rabbitMQPackage.7.+.tar.gz 	deleted:    rabbitMQ/rabbitMQPackage.8.+.tar.gz 	deleted:    rabbitMQ/rabbitMQPackage.9.+.tar.gz 	deleted:    service.ini 	modified:   testRabbitMQ.ini

--- deployment/deployment.php
+++ deployment/deployment.php
@@ -6,23 +6,25 @@ require_once('rabbitMQLib.inc');
 require_once('handleUsers.php.inc');
 //require_once('sendDisLog.php');
 
-function doDatabaseTransaction($request){
-  $client = new rabbitMQClient("testRabbitMQ.ini","dbServer");
-  $response = $client->send_request($request);
-  return $response;
+
+
+function markBad ($dir, $file){
+	$explode = explode(".", $file);
+	shell_exec("mv $dir" .$file ." $dir" ."$explode[0].$explode[1].-.tar.gz");
 }
 
-function doUpdate($request){
-	$client = new rabbitMQClient("testRabbitMQ.ini", $request['server']);
-
-}
-
-function doRollback($request){
-
-}
-
-function doService($service, $server){
-	return exec("sudo systemctl $service $server");
+function getRecentGood ($dir){
+	exec("ls $dir", $list);
+        $other = 0;
+        for($i = 0; $i < count($list); $i++){
+                $explode = explode(".", $list[$i]);
+        	$version = $explode[1];
+		if($version > $other && $explode[2] == "+"){
+                	$other = $version;
+        	}
+        }
+        exec("ls $dir | grep $other", $output);
+	return $output[0];
 }
 
 function recentVersion($dir){
@@ -75,7 +77,7 @@ function deleteFile($dir){
 		$version = $explode[1];
 		if($version > $other){
 			$other = $version;
-		}
+		}	
 		if($other-- == $version){
 			$output = shell_exec("ls $dir | grep $other.'+'");
 		}
@@ -85,7 +87,7 @@ function deleteFile($dir){
 	//return $output;
 }
 
-print_r(deleteFile("frontEnd"));
+//print_r(deleteFile("frontEnd"));
 
 function requestProcessor($request)
 {
@@ -119,12 +121,13 @@ function requestProcessor($request)
 				break;
 			case "DMZ":
 				//$client = new rabbitMQClient("testRabbitMQ.ini", "DMZ.prod");
-				$fileName = recentVersion("DMZ");
-				$path = "DMZ/$fileName ubuntu@10.0.1.something: something";
+				$fileName = recentVersion("DMZ.prod");
+				$path = "DMZ/$fileName ubuntu@10.0.1.81:~git/IT490202/dmzService/packages";
 				break;
 			default:
 				break;
 		}
+		var_dump($path);
 		shell_exec("scp " .$path);
 		$client->send_request($request);
 		return "Successfully received and sent";
@@ -136,32 +139,33 @@ function requestProcessor($request)
                         case "rabbitMQ":
                                 $client = new rabbitMQClient("testRabbitMQ.ini", "rabbitMQ.prod");
 				
-				$version = versionNum("rabbitMQ/");
-				$versionFile = "rabbitMQ/rabbitMQPackage.$version.+.tar.gz";
-				$versionPath = "rabbitMQ/$versionFile ubuntu@10.0.1.216:~/git/it490/packages";
-
-				$fileName = parseFiles("rabbitMQ/");
+				$version = getRecentGood("rabbitMQ/");
+				markBad("rabbitMQ/", $version);
+				$fileName = getRecentGood("rabbitMQ/");
                                 $path = "rabbitMQ/$fileName ubuntu@10.0.1.216:~/git/it490/packages";
                                 break;
                         case "frontEnd":
                                 $client = new rabbitMQClient("testRabbitMQ.ini", "frontEnd.prod");
 				
-				$version = versionNum("frontEnd/");
-				$versionFile = "frontEnd/frontEndPackage.$version.+.tar.gz";
-				$versionPath = "frontEnd/$versionFile ubuntu@10.0.1.142:~git/IT4902020/packages";
+				$version = recentVersion("frontEnd/");
+				markBad("frontEnd/", $version);
+				$fileName = getRecentGood("frontEnd/");
+				$path = "frontEnd/$fileName ubuntu@10.0.1.142:~/git/IT4902020/packages";
 				break;
                         case "DMZ":
                                 $client = new rabbitMQClient("testRabbitMQ.ini", "DMZ.prod");
 
-				$version = versionNum("DMZ/");
-				$versionFile = "DMZ/DMZPackage.$version.+.tar.gz";
-				$versionPath = "DMZ/$versionFile ubuntu@something:something";
+				$version = recentVersion("DMZ/");
+				markBad("DMZ/", $version);
+				$fileName = getRecentGood("DMZ/");
+				$path = "DMZ/$fileName ubuntu@10.0.1.81:~git/IT4902020/packages";
 				break;
                         default:
                                 break;
                 }
-		shell_exec("scp " .$versionPath);
-		return "no";
+		shell_exec("scp " .$path);
+		$explode = explode(".", $fileName);
+		return $explode[1];
 	case "service":
 		//Deployment controls systemd service
 		switch ($request['server'])
